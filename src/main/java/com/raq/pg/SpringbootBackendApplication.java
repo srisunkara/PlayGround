1e13222b1e39cdb080143c8b81d7aab8b9272f56
Sri Sunkara <sunkarsr@yahoo.com>
2024-01-27 17:44:00 -0800

    Initial commit

---
+++ src/main/java/com/raq/pg/SpringbootBackendApplication.java
@@ -0,0 +1,27 @@
+package com.raq.pg;
+
+import com.raq.pg.model.User;
+import com.raq.pg.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.userRepository.save(new User("Ramesh", "Fadatare", "ramesh@gmail.com"));
+        this.userRepository.save(new User("Tom", "Cruise", "tom@gmail.com"));
+        this.userRepository.save(new User("Tony", "Stark", "tony@gmail.com"));
+    }
+}
---
+++ src/main/java/com/raq/pg/utils/StringExtractor.java
@@ -0,0 +1,57 @@
+package com.raq.pg.utils;
+
+import java.util.Stack;
+
+public class StringExtractor {
+
+    public static String extractString(String inString, char openChar, char endChar) {
+
+        if (inString == null) {
+            return null;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        int start = -1;
+
+        for (int i = 0; i < inString.length(); i++) {
+            char currentChar = inString.charAt(i);
+
+            if (currentChar == openChar) {
+                if (start == -1) {
+                    start = i;
+                }
+                stack.push(currentChar);
+            } else if (currentChar == endChar) {
+                if (stack.isEmpty()) {
+                    return null; // Unmatched closing brace
+                }
+                stack.pop();
+                if (stack.isEmpty()) {
+                    return inString.substring(start + 1, i);
+                }
+            }
+        }
+
+        return null; // No matching pair found
+    }
+
+    public static void main(String[] args) {
+        char openChar = '{';
+        char endChar = '}';
+        String text = "{a{b{c}}d}";
+        String extractedString = extractString(text, openChar, endChar);
+        System.out.println("Extracted string: " + extractedString);
+
+        extractedString = extractString(extractedString, openChar, endChar);
+        System.out.println("Extracted string: " + extractedString);
+
+        extractedString = extractString(extractedString, openChar, endChar);
+        System.out.println("Extracted string: " + extractedString);
+
+        extractedString = extractString(extractedString, openChar, endChar);
+        System.out.println("Extracted string: " + extractedString);
+
+        extractedString = extractString(extractedString, openChar, endChar);
+        System.out.println("Extracted string: " + extractedString);
+    }
+}
